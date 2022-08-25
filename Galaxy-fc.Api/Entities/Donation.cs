c307f1d8154f9574a2f064a89d77a4acba1ee935
Stephen Cherry <stephen.m.cherry91@gmail.com>
2022-08-25 07:37:47 -0400

    Add project files.

---
+++ Galaxy-fc.Api/Entities/Donation.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Galaxy_fc.Api.Entities
+{
+	public class Donation
+	{
+		public int Id { get; set; }
+		public int DonationId { get; set; }
+        [Required]
+        public string UserName { get; set; } = null!;
+        public int Gil { get; set; }
+        public DateTime Created_At { get; set; }
+        public DateTime Modified_At { get; set; }
+    }
+}

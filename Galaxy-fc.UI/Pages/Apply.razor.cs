3d4ea1e210e00193a3e5c1ea481a621396a7bf38
Stephen Cherry <stephen.m.cherry91@gmail.com>
2022-08-25 18:11:05 -0400

    Overhauled Form and added Group Pic on Home

---
+++ Galaxy-fc.UI/Pages/Apply.razor.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Galaxy_fc.UI.Pages
+{
+    public class Application
+    {
+        [Required(ErrorMessage = "Your character name is required.")]
+        public string? CharacterName { get; set; }
+        [Required(ErrorMessage = "Your discord name is required.")]
+        public string? DiscordName { get; set; }
+        [Required(ErrorMessage = "Your current age is required.")]
+        public string? Age { get; set; }
+        [Required(ErrorMessage = "The typical time you play is required.")]
+        public string? Time { get; set; }
+        [Required(ErrorMessage = "An explaination of what your expectations are is required.")]
+        public string? Expectations { get; set; }
+        [Required(ErrorMessage = "An answer to this is required.")]
+        public string? Acceptance { get; set; }
+        [Required(ErrorMessage = "Your opinion of the above statement is required.")]
+        public string? Opinion { get; set; }
+    }
+}
